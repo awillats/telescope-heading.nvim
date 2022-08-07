d5e0c471500e862aca2190c33f7a3630389c0168
Adam Willats <awillats3@gatech.edu>
2022-08-07 12:49:35 -0700

    Support headings in vimrc / init.lua files
    assumes multiples of " used for headings (akin to ## heading in markdown)

---
+++ lua/telescope/_extensions/heading/format/vimrc.lua
@@ -0,0 +1,33 @@
+local Vimrc = {}
+
+function Vimrc.get_headings(filepath, start, total)
+    local headings = {}
+    local index = start
+    local matches = {
+        '"" ',
+        '""" ',
+        '"""" ',
+        '""""" ',
+        '"""""" ',
+    }
+    while index <= total do
+        local line = vim.fn.getline(index)
+        -- match heading
+        for _, pattern in pairs(matches) do
+            if vim.startswith(line, pattern) then
+                table.insert(headings, {
+                    heading = vim.trim(line),
+                    line = index,
+                    path = filepath,
+                })
+                break
+            end
+        end
+
+        ::next::
+        index = index + 1
+    end
+    return headings
+end
+
+return Vimrc
